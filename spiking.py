d478baaba787bf5f56fea944267bb408206ff6bd
samfok <samfok@stanford.edu>
2018-04-26 17:06:33 -0700

    update spiking

--- spiking.py
+++ spiking.py
@@ -5,42 +5,48 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 class Spikes():
-    def __init__(self, ax, window=2, t_max=5, dt=0.02):
+    def __init__(self, ax, window=2, t_max=10, dt=0.02):
         self.ax = ax
         self.dt = dt
-        self.t_max = t_max
+        self.w_size = window
+
         self.t_data = np.arange(0, 2*t_max, dt)
         self.y_data = np.random.sample(len(self.t_data))
 
+        self.line = Line2D([], [])
+        self.ax.add_line(self.line)
+
+        self._reset()
+
+    def _reset(self):
+        """Reset the animation state"""
+        self.w_idx = np.array([0, 1], dtype=int)
         self.t_display = [self.t_data[0]]
         self.y_display = [self.y_data[0]]
+        self.ax.set_xlim(0, self.w_size)
 
-        self.line = Line2D(self.t_display, self.y_display)
-        self.ax.add_line(self.line)
-        self.w_idx = np.array([0, 1], dtype=int) # window indices
-        self.window = [0, window]
-        self.ax.set_xlim(self.window[0], self.window[1])
-
-    def update(self, y):
-        if self.t_display[-1] < self.window[1]:
-            self.w_idx[1] += 1
-            self.t_display = self.t_data[self.w_idx[0]:self.w_idx[1]]
-            self.y_display = self.y_data[self.w_idx[0]:self.w_idx[1]]
-        else:
-            self.w_idx += 1
-            self.t_display = self.t_data[self.w_idx[0]:self.w_idx[1]]
-            self.y_display = self.y_data[self.w_idx[0]:self.w_idx[1]]
-            self.window[0] = self.t_display[0]
-            self.window[1] = self.t_display[1]
-            self.ax.set_xlim(self.window[0], self.window[1])
-
+    # def update(self, w_idx, w_update):
+    def update(self, w_params):
+        """Update the animation"""
+        w_idx, w_update = w_params
+        self.t_display = self.t_data[w_idx[0]:w_idx[1]]
+        self.y_display = self.y_data[w_idx[0]:w_idx[1]]
         self.line.set_data(self.t_display, self.y_display)
+        if w_update:
+            self.ax.set_xlim(self.t_display[0], self.t_display[-1])
         self.ax.figure.canvas.draw()
         return self.line,
 
-    def get_framecount(self):
-        framecount = int(np.ceil(self.t_max/self.dt))
-        return framecount
+    def w_idx_generator(self):
+        while self.t_display[-1] != self.t_data[-1]:
+            if self.t_display[-1]-self.t_display[0] < self.w_size:
+                self.w_idx[1] += 1
+                update_window = False
+            else:
+                self.w_idx += 1
+                update_window = True
+            yield (self.w_idx, update_window)
+        self._reset()
 
 # Fixing random state for reproducibility
 np.random.seed(19680801)
@@ -49,6 +55,7 @@ fig, ax = plt.subplots()
 spikes = Spikes(ax)
 
 ani = animation.FuncAnimation(
-    fig, spikes.update, spikes.get_framecount(), interval=10, blit=True)
+    fig, spikes.update, spikes.w_idx_generator, interval=10, blit=True, repeat=False)
 
-plt.show()
+ani.save("test.gif")
+# plt.show()
